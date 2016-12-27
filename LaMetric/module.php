74e5382ad3574f4dfba01b5abb807291121e27cb
Dennis Deitermann <Dennis@imac.deitermann.home>
2016-12-27 20:56:23 +0100

    added module.php

---
+++ LaMetric/module.php
@@ -0,0 +1,15 @@
+<?
+    // Klassendefinition
+    class ModulnameXYZ extends IPSModule {
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion() {
+            echo $this->InstanceID;
+        }
+    }
+?>
