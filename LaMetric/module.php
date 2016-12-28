3bcc0b4ca1ce6fff75ef6b1054a7d6de9e063916
Tuxos <dennis@deitermann.org>
2016-12-28 13:09:03 +0100

    Updated module.php

--- LaMetric/module.php
+++ LaMetric/module.php
@@ -14,9 +14,19 @@
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
 
+	    $this->RegisterProfileEx("LaMetric-brightness-mode", "Light", "", "", 0 /* Boolean */, Array(
+							Array(0, "Manual", "", 16711680),
+							Array(1, "Auto", "", 65280)
+						));
+
             $id = $this->RegisterVariableString("osversion", "OS Version", "~String",0);
             $id = $this->RegisterVariableString("ssid", "SSID", "~String",1);
-            $id = $this->RegisterVariableInteger("wlanconnection", "WLan Empfang", "~Intensity.100",2);
+	    $id = $this->RegisterVariableInteger("wlanconnection", "WLan Empfang", "~Intensity.100",2);
+	    $id = $this->RegisterVariableBoolean("bluetooth", "Bluetooth", "~Switch",3);
+	    $id = $this->RegisterVariableString("bluetoothname", "Bluetooth Name", "~String",4);
+	    $id = $this->RegisterVariableInteger("volume", "Volume", "~Intensity.100",5);
+	    $id = $this->RegisterVariableInteger("brightness", "Helligkeit", "~Intensity.100",6);
+	    $id = $this->RegisterVariableInteger("brightnessmode", "Helligkeit Modus", "LaMetric-brightness-mode",7);
 
         }
  
