9107da6040ac47551c32494001d74c150688e533
Tuxos <dennis@deitermann.org>
2016-12-28 16:59:18 +0100

    Update

--- LaMetric/module.php
+++ LaMetric/module.php
@@ -26,7 +26,7 @@
 
         }
  
-        public function readdata(integer: $modulid) {
+        public function readdata(integer $modulid) {
 
 		$ip = $this->ReadPropertyString("ipadress");	
 		$apikey = $this->ReadPropertyString("apikey");
