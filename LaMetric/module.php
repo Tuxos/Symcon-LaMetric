c45f0fd2bf007aed861a1620bd4dbe40eb54c221
Tuxos <dennis@deitermann.org>
2016-12-28 16:39:47 +0100

    Update

--- LaMetric/module.php
+++ LaMetric/module.php
@@ -26,7 +26,7 @@
 
         }
  
-        public function readdata() {
+        public function readdata($parentid) {
 	
 		$ip = $this->ReadPropertyString("ipadress");	
 		$apikey = $this->ReadPropertyString("apikey");
@@ -56,7 +56,8 @@
 
 	         if ($data->display->brightness_mode == "auto") { $mode=true; } else { $mode=false; };
 
-                 $nameid = IPS_GetVariableIDByName("Name", $this);
+
+                 $nameid = IPS_GetVariableIDByName("Name", $parentid);
 
 	         // SetValue(24351 /*[Devices\LaMetric\LaMetric Esszimmer\Volume]*/, $data->audio->volume);
 	         // SetValue(14287 /*[Devices\LaMetric\LaMetric Esszimmer\Helligkeit]*/,$data->display->brightness);
