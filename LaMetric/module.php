5b93fcac88afac3a416ced4da170d1b975a1b8d2
Tuxos <dennis@deitermann.org>
2016-12-28 13:42:58 +0100

    Updates

--- LaMetric/module.php
+++ LaMetric/module.php
@@ -5,30 +5,24 @@
             // Diese Zeile nicht löschen.
             parent::Create();
 
-            $this->RegisterPropertyString("ipadress", "192.168.1.99");
-            $this->RegisterPropertyString("apikey", "884d3d4b48b979092f2b0b866efd6a6c6f3412d2dcfd454ce747359ed1fb99c1");
+            $this->RegisterPropertyString("ipadress", "");
+            $this->RegisterPropertyString("apikey", "");
  
         }
 
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
-		
-            // $this->RegisterProfile(1,"WITHINGS_M_Groesse"  ,"Gauge"  ,""," cm");
 
-	    //$this->RegisterProfile(0, "LaMetric-brightness-mode", "Light", "", "", Array(
-		//					Array(0, "Manual", "", 0x0000FF),
-		//					Array(1, "Auto", "", 0xFF0000)
-		//				));
-
-            $id = $this->RegisterVariableString("osversion", "OS Version", "~String",0);
-            $id = $this->RegisterVariableString("ssid", "SSID", "~String",1);
-	    $id = $this->RegisterVariableInteger("wlanconnection", "WLan Empfang", "~Intensity.100",2);
-	    $id = $this->RegisterVariableBoolean("bluetooth", "Bluetooth", "~Switch",3);
-	    $id = $this->RegisterVariableString("bluetoothname", "Bluetooth Name", "~String",4);
-	    $id = $this->RegisterVariableInteger("volume", "Volume", "~Intensity.100",5);
-	    $id = $this->RegisterVariableInteger("brightness", "Helligkeit", "~Intensity.100",6);
-	    $id = $this->RegisterVariableBoolean("brightnessmode", "Helligkeit Modus", "LaMetric-brightness-mode",7);
+	    $id = $this->RegisterVariableString("name", "Name", "~String",0);
+            $id = $this->RegisterVariableString("osversion", "OS Version", "~String",1);
+            $id = $this->RegisterVariableString("ssid", "SSID", "~String",2);
+	    $id = $this->RegisterVariableInteger("wlanconnection", "WLan Empfang", "~Intensity.100",3);
+	    $id = $this->RegisterVariableBoolean("bluetooth", "Bluetooth", "~Switch",4);
+	    $id = $this->RegisterVariableString("bluetoothname", "Bluetooth Name", "~String",5);
+	    $id = $this->RegisterVariableInteger("volume", "Volume", "~Intensity.100",6);
+	    $id = $this->RegisterVariableInteger("brightness", "Helligkeit", "~Intensity.100",7);
+	    $id = $this->RegisterVariableBoolean("brightnessautomode", "Helligkeit Auto Modus", "~Switch",8);
 
         }
  
@@ -42,32 +36,5 @@
         public function MeineErsteEigeneFunktion() {
             // Selbsterstellter Code
         }
-	    
-    //**************************************************************************
-    //  0 - Bool
-    //  1 - Integer
-    //  2 - Float
-    //  3 - String
-    //**************************************************************************    
-    protected function RegisterProfile($Typ, $Name, $Icon, $Prefix, $Suffix, $MinValue=false, $MaxValue=false, $StepSize=false, $Digits=0) 
-      {
-      if(!IPS_VariableProfileExists($Name)) 
-        {
-        IPS_CreateVariableProfile($Name, $Typ);  
-        } 
-      else 
-        {
-        $profile = IPS_GetVariableProfile($Name);
-        if($profile['ProfileType'] != $Typ)
-          throw new Exception("Variable profile type does not match for profile ".$Name);
-        }
-        
-      IPS_SetVariableProfileIcon($Name, $Icon);
-      IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
-      IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
-		  if ( $Typ == 2 )
-			 IPS_SetVariableProfileDigits($Name, $Digits);
-      }
-	    
     }
 ?>
