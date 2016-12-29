14eb6d71d4d7d61d0072c0e0e75d26845392a98e
Tuxos <dennis@deitermann.org>
2016-12-29 04:30:34 +0100

    Update

--- LaMetric/module.php
+++ LaMetric/module.php
@@ -28,7 +28,6 @@
 	$id = $this->RegisterVariableInteger("brightness", "Helligkeit", "~Intensity.100",7);
 	$id = $this->RegisterVariableBoolean("brightnessautomode", "Helligkeit Auto Modus", "~Switch",8);
 	
-	$this->readdata();
 	$this->RegisterTimer('ReadData', $intervall, 'LM_readdata($id)');
 
 //	$parentid = IPS_GetParent($id);
@@ -41,11 +40,39 @@
 	if (($this->ReadPropertyString("ipadress") != "") and ($this->ReadPropertyString("apikey") != ""))
 		{
 			$this->SetStatus(102);
+			// $this->readdata();
 		}
 
 	}
  
+// Register Time Function
 
+	protected function RegisterTimer($ident, $interval, $script) {
+
+		$id = @IPS_GetObjectIDByIdent($ident, $this->InstanceID);
+		if ($id && IPS_GetEvent($id)['EventType'] <> 1) {
+			IPS_DeleteEvent($id);
+			$id = 0;
+			}
+		if (!$id) {
+		$id = IPS_CreateEvent(1);
+		IPS_SetParent($id, $this->InstanceID);
+		IPS_SetIdent($id, $ident);
+		}
+		
+		IPS_SetName($id, $ident);
+		IPS_SetHidden($id, true);
+		IPS_SetEventScript($id, "\$id = \$_IPS['TARGET'];\n$script;");
+		if (!IPS_EventExists($id)) throw new Exception("Ident with name $ident is used for wrong object type");
+		
+		if (!($interval > 0)) {
+			IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
+			IPS_SetEventActive($id, false);
+		} else {
+			IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $interval);
+			IPS_SetEventActive($id, true);
+		}
+	}
 
 	// Lese alle Configurationsdaten aus
 
