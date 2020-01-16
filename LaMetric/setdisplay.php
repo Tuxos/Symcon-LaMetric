58a9cf603f48d30b28e5d225a2f10920a2382ec3
Dennis Deitermann <Tuxos@users.noreply.github.com>
2020-01-16 20:50:48 +0100

    Update setdisplay.php

--- LaMetric/setdisplay.php
+++ LaMetric/setdisplay.php
@@ -5,6 +5,7 @@
 	$parentid = IPS_GetParent($_IPS['VARIABLE']);
 	$helligkeitid = IPS_GetObjectIDByName('Helligkeit', $parentid);
 	$modeid = IPS_GetObjectIDByName('Helligkeit Auto Modus', $parentid);
+	$screensaverid = IPS_GetObjectIDByName('Screensaver', $parentid);
 	$mode = GetValueBoolean($modeid);
 	$helligkeit = GetValueInteger($helligkeitid);
 	$screensaver = GetValueBoolean($screensaverid);
