464f5a138af1437ccd5f06c3344bdc435b6dda2b
Dennis Deitermann <Tuxos@users.noreply.github.com>
2020-01-16 20:21:08 +0100

    Update module.php

--- LaMetric/module.php
+++ LaMetric/module.php
@@ -329,7 +329,7 @@
 	}
 
 	// Display Konfiguration
-	public function display($helligkeit, $modus, $screensaver) {
+	public function display($helligkeit, boolean $modus, boolean $screensaver) {
 
 		$ip = $this->ReadPropertyString("ipadress");
 		$url = "http://".$ip.":8080/api/v2/device/display";
