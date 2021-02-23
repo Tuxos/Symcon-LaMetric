896a853b9cda8779fb2049b5e131485505b7921f
Dennis Deitermann <Tuxos@users.noreply.github.com>
2021-02-23 22:06:00 +0100

    Update module.php

--- LaMetric/module.php
+++ LaMetric/module.php
@@ -162,7 +162,7 @@
 		SetValue(IPS_GetObjectIDByName("Bluetooth Name", $this->InstanceID),$data->bluetooth->name);
 		SetValue(IPS_GetObjectIDByName("Name", $this->InstanceID),$data->name);
 		SetValue(IPS_GetObjectIDByName("OS Version", $this->InstanceID),$data->os_version);
-		SetValue(IPS_GetObjectIDByName("SSID", $this->InstanceID),$data->wifi->ssid);
+		SetValue(IPS_GetObjectIDByName("SSID", $this->InstanceID),$data->wifi->essid);
 		SetValue(IPS_GetObjectIDByName("WLan Empfang", $this->InstanceID),$data->wifi->signal_strength);
 
 		if (file_exists(IPS_GetKernelDir()."/scripts/LM_setdisplay.php") == false) {
